25552df2015bedfdbb0c7f5f6eedc8fe23028c77
aguinazu-ds <aeguinazu@gmail.com>
2022-02-02 18:31:38 -0300

    cambio nombre variable de response a respuesta

--- get-master-lor-data.py
+++ get-master-lor-data.py
@@ -1,19 +1,18 @@
 import json
-from urllib import response
 import requests
 
 api_token = 'RGAPI-2a309c08-fd0c-4bbf-9359-5e84a73f2563'
 api_url_base = 'https://americas.api.riotgames.com/lor/ranked/v1/leaderboards'
 
-response = requests.get('{}?api_key={}'.format(api_url_base,api_token))
+respuesta = requests.get('{}?api_key={}'.format(api_url_base,api_token))
 
 def get_ranked_info():
     ''' '''
     api_url = '{}?api_key={}'.format(api_url_base,api_token)
-    response = requests.get(api_url)
+    respuesta = requests.get(api_url)
 
-    if response.status_code == 200:
-        return json.loads(response.content.decode('utf-8'))
+    if respuesta.status_code == 200:
+        return json.loads(respuesta.content.decode('utf-8'))
     else:
         return None
 
