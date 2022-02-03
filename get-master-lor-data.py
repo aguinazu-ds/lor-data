b56a4b718dbea9122dd8d970a57af3d00744a399
aguinazu-ds <aeguinazu@gmail.com>
2022-02-02 21:59:40 -0300

    Modificación a archivo get-master-lor-data.py para obtener datos de America, Europa y SEA

--- get-master-lor-data.py
+++ get-master-lor-data.py
@@ -2,13 +2,12 @@ import json
 import requests
 
 api_token = 'RGAPI-2a309c08-fd0c-4bbf-9359-5e84a73f2563'
-api_url_base = 'https://americas.api.riotgames.com/lor/ranked/v1/leaderboards'
+check = requests.get('https://americas.api.riotgames.com/lor/ranked/v1/leaderboards?api_key={}'.format(api_token))
 
-respuesta = requests.get('{}?api_key={}'.format(api_url_base,api_token))
-
-def get_ranked_info():
+def get_ranked_info(region):
     ''' '''
-    api_url = '{}?api_key={}'.format(api_url_base,api_token)
+    api_url_base = 'https://{}.api.riotgames.com/lor/ranked/v1/leaderboards'
+    api_url = '{}?api_key={}'.format(api_url_base.format(region),api_token)
     respuesta = requests.get(api_url)
 
     if respuesta.status_code == 200:
@@ -16,7 +15,11 @@ def get_ranked_info():
     else:
         return None
 
-data = get_ranked_info()
-
-with open('data/data.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+if check.status_code == 200:
+    data_americas, data_europe, data_sea = get_ranked_info('americas'), get_ranked_info('europe'), get_ranked_info('sea')
+    with open('data/data_americas.json', 'w', encoding='utf-8') as f:
+        json.dump(data_americas, f, ensure_ascii=False, indent=4)
+    with open('data/data_europe.json', 'w', encoding='utf-8') as f:
+        json.dump(data_europe, f, ensure_ascii=False, indent=4)
+    with open('data/data_sea.json', 'w', encoding='utf-8') as f:
+        json.dump(data_sea, f, ensure_ascii=False, indent=4)
